7bd21846704adefa310aebf080c6775db3bfd7f1
Nguyễn Tiến Dũng <1stnguyendung@gmail.com>
2022-09-17 12:28:14 +0700

    hihi

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+
+$mbb = new GaDev\MBBank\MBBank('dungadeptry', 'Nguyendung27112005@@');
+
+$mbb->doLogin();
--- src/mbbank.php
+++ src/mbbank.php
@@ -6,15 +6,20 @@ namespace GaDev\MBBank;
 class MBBank {
 
 
-    private $username;
+    private string $username;
 
-    private $password;
+    private string $password;
 
-    private ?string $msgType = null;
+    private $config = array();
 
     private array $msgTypes = [
         'LOGIN_MSG' => 'https://mobile.mbbank.com.vn/retail_lite/common/doLogin',
     ];
+
+    public function __construct(string $username, string $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
 
     private $header = array(
         'User-Agent: Mozilla/5.0 (Linux; Android 7.1.2; A5010 Build/N2G48H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.120 Mobile Safari/537.36',
@@ -25,7 +30,10 @@ class MBBank {
     );
 
     public function doLogin() {
-        
+        $Data = json_encode(array(
+            'refNo' => $this->config['refNo'],
+        ));
+        return $this->CURL($this->msgTypes['LOGIN_MSG'], $this->header, $Data);
     }
 
     private function CURL($Action, $header, $data = '')
@@ -50,6 +58,21 @@ class MBBank {
             return json_decode($body, true);
         }
         return $body;
+    }
+
+    private function get_TOKEN() :string
+    {
+        return  $this->generateRandom(22) . ':' . $this->generateRandom(9) . '-' . $this->generateRandom(20) . '-' . $this->generateRandom(12) . '-' . $this->generateRandom(7) . '-' . $this->generateRandom(7) . '-' . $this->generateRandom(53) . '-' . $this->generateRandom(9) . '_' . $this->generateRandom(11) . '-' . $this->generateRandom(4);
+    }
+
+    private function generateRandom($length = 20)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
     }
 }
 
